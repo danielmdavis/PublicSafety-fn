86fb59c521a4241fdaf9e72b8f67e46a412d5365
Shane Landrum <shane.landrum@oracle.com>
2019-06-14 16:45:21 -0400

    Adjust build command to actually *be* verbose

--- buildme.sh
+++ buildme.sh
@@ -2,7 +2,7 @@
 
 # Work around the fact that fn build doesn't take an --all param yet
 for i in EvaluateImage HandleDroneImage OpenTicket PullAndCheck TellDroneAboutTicket UpdateTicket; do \
-  fn build -v $i; \
+  fn -v build $i; \
 done
 
 fn deploy --all --local
