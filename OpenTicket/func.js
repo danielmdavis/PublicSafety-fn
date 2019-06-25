5d57336f99bbde882df54cfa1a93d7b309579ab5
Daniel Davis <danis1911@gmail.com>
2019-06-25 11:34:42 -0400

    api call added, dockerfile edited for OpenTicket

--- OpenTicket/func.js
+++ OpenTicket/func.js
@@ -1,9 +1,34 @@
 const fdk=require('@fnproject/fdk');
 
 fdk.handle(function(input){
-  let data = 'no input received'
-  if (input.data) {
-    data = input.data;
-  }
-  return {'message': 'the recorded temp is: ' + name}
+ 
+  var request = require("request");
+
+  var options = {
+    method: 'POST',
+    url: 'https://BurlingtonHUB-orasenatdpltintegration01.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/PUBLIC_SAFETY_V2/1.0/metadata/ticket',
+    headers:
+    {
+      'Postman-Token': 'e254cfd3-723c-4eea-a50c-4bf03d4dce0a',
+      'cache-control': 'no-cache',
+      Authorization: 'Basic Y2xvdWQuYWRtaW46I0FCQ0RlZmdoMTIzNCM=',
+      'Content-Type': 'application/json'
+    },
+    body:
+    {
+      timestamp: '13-06-19',
+      temperature: '489.2',
+      location: 'Burlington, MA',
+      id: '211',
+      status: '1'
+    },
+    json: true
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+  });
+
+  return {body}
 })
