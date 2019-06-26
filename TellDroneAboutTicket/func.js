ab9c50c9b06c484ea01c2664f20242c86289bf2b
Shane Landrum <shane.landrum@oracle.com>
2019-06-26 13:50:09 -0400

    Basic code for notifying drone about fire location. Needs debugging for fn response.

--- TellDroneAboutTicket/func.js
+++ TellDroneAboutTicket/func.js
@@ -1,11 +1,40 @@
 const fdk=require('@fnproject/fdk');
+const request=require('request');
 
-fdk.handle(function(input){
-  let data = 'no ticket number received'
-  if (input.data) {
-    data = input.data;
-  }
-  // tell the drone the ticket number.
-  
-  return {'message': 'the ticket number is: ' + data}
-})
+
+function RESTcaller (inputId, context) {
+  var options = {
+    method: 'POST',
+    uri: 'https://burlingtonhub-orasenatdpltintegration01.integration.ocp.oraclecloud.com/ic/api/integration/v1/flows/rest/DRONETRIGGER/1.0/dronetrigger',
+    headers:
+    {
+      'Postman-Token': 'e254cfd3-723c-4eea-a50c-4bf03d4dce0a',
+      'cache-control': 'no-cache',
+      'Authorization': 'Basic Y2xvdWQuYWRtaW46I0FCQ0RlZmdoMTIzNCM=',
+      'Content-Type': 'application/json'
+    },
+    body:
+    {
+      "Location":"Building1",
+      "Temperature1":"45",
+      "TimeStamp":"12:12:12"
+    },
+    json: true
+  };
+
+  return new Promise( function (resolve, reject) {
+    request(options, function (error, response, body) {
+      if (err) return reject(err);
+      try {
+        resolve(body);
+      } catch(e) {
+        reject(e);
+      }
+    });
+  });
+}
+
+
+fdk.handle(function (input, ctx) {
+  return RESTcaller(input);
+});
