14fba96b4d6ce01716ae5a202a9df82eda05ae59
Shane Landrum <shane.landrum@oracle.com>
2019-07-01 07:21:04 -0400

    Restoring code from elsewhere

--- OpenTicket/func.js
+++ OpenTicket/func.js
@@ -2,24 +2,23 @@ const fdk=require('@fnproject/fdk');
 const request=require('request');
 
 
-function RESTcaller (inputId, context) {
+function RESTcaller (input) {
   var options = {
     method: 'POST',
     uri: 'https://BurlingtonHUB-orasenatdpltintegration01.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/PUBLIC_SAFETY_V2/1.0/metadata/ticket',
     headers:
     {
-      'Postman-Token': 'e254cfd3-723c-4eea-a50c-4bf03d4dce0a',
       'cache-control': 'no-cache',
       'Authorization': 'Basic Y2xvdWQuYWRtaW46I0FCQ0RlZmdoMTIzNCM=',
       'Content-Type': 'application/json'
     },
     body:
     {
-      timestamp: '13-06-19',
-      temperature: '489.2',
-      location: 'Burlington, MA',
-      id: inputId,
-      status: '1'
+      timestamp: input.timestamp,     // '13-06-19 12:12:12'
+      temperature: input.temperature, // '489.2'
+      location: input.location,       // 'Burlington, MA'
+      id: input.id,                    // an integer
+      status: input.status            // '1'
     },
     json: true
   };
@@ -40,6 +39,6 @@ function RESTcaller (inputId, context) {
 // When I call RESTcaller(223) by itself, it runs appropriately.
 // When I wrap it in fdk.handle (as below), the request happens, but the function never returns.
 
-fdk.handle(function (input, ctx) {
-  return RESTcaller(input);
+fdk.handle(function (inputData, ctx) {
+  return RESTcaller(inputData);
 });
