6745fdf116bd79c364ff70ec91d1a7e456ff30d0
Shane Landrum <shane.landrum@oracle.com>
2019-07-01 15:01:07 -0400

    Add logging statements

--- TellDroneAboutTicket/func.js
+++ TellDroneAboutTicket/func.js
@@ -23,10 +23,14 @@ function RESTcaller (inputId, context) {
 
   return new Promise( function (resolve, reject) {
     request(options, function (error, response, body) {
-      if (err) return reject(err);
+      if (error) {
+        console.log("error in request: " + err);
+        return reject(error);
+      }
       try {
         resolve(body);
       } catch(e) {
+        console.log("error in catch: " + e);
         reject(e);
       }
     });
