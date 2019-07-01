93620e7c3b617b5eb5869ef49f553a16ed7375ac
Shane Landrum <shane.landrum@oracle.com>
2019-07-01 07:25:05 -0400

    Basic code for updating a ticket

--- UpdateTicket/func.js
+++ UpdateTicket/func.js
@@ -1,15 +1,48 @@
 const fdk=require('@fnproject/fdk');
+const request=require('request');
 
-fdk.handle(function(ticketid, message){
-  let message = 'empty update'
-  if (input.ticketid) {
-    ticketid = input.ticketid;
-  }
-  if (input.message) {
-    message = input.message;
-  }
-  // TODO: need some error handling around input not being passed in
-  // TODO: call NetSuite and update the ticket.
+# UpdateTicket
+#
+# Updates a Netsuite ticket about a potential fire with new information.
+# TODO: how does this code need to be different than OpenTicket?
 
-  return {'message': 'the ticket update message is: ' + data}
-})
+function RESTcaller (input) {
+  var options = {
+    method: 'POST',
+    uri: 'https://BurlingtonHUB-orasenatdpltintegration01.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/PUBLIC_SAFETY_V2/1.0/metadata/ticket',
+    headers:
+    {
+      'cache-control': 'no-cache',
+      'Authorization': 'Basic Y2xvdWQuYWRtaW46I0FCQ0RlZmdoMTIzNCM=',
+      'Content-Type': 'application/json'
+    },
+    body:
+    {
+      timestamp: input.timestamp,     // '13-06-19 12:12:12'
+      temperature: input.temperature, // '489.2'
+      location: input.location,       // 'Burlington, MA'
+      id: input.id,                    // an integer
+      status: input.status            // '1'
+    },
+    json: true
+  };
+
+  return new Promise( function (resolve, reject) {
+    request(options, function (error, response, body) {
+      if (err) return reject(err);
+      try {
+        resolve(body);
+      } catch(e) {
+        reject(e);
+      }
+    });
+  });
+}
+
+
+// When I call RESTcaller(223) by itself, it runs appropriately.
+// When I wrap it in fdk.handle (as below), the request happens, but the function never returns.
+
+fdk.handle(function (inputData, ctx) {
+  return RESTcaller(inputData);
+});
