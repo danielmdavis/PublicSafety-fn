3bfba0ed5f73d132118a2f206ba9df9294370f9b
Shane Landrum <shane.landrum@oracle.com>
2019-07-11 10:05:22 -0400

    This doesn't work, about to rip out the request stuff and replace it with fetch

--- KickoffDrone/func.js
+++ KickoffDrone/func.js
@@ -1,9 +1,63 @@
-const fdk=require('@fnproject/fdk');
+const fdk=require('@fnproject/fdk')
+const http = require("http")
+const fetch = require('node-fetch')
+
+// invoke the drone to start flying.
+
+async function callDrone(hostname, remark) {
+  //console.log('hostname will be ' + hostname.split('.')[:])
+  var options = {
+    "method": "POST",
+    "hostname": hostname,
+    "path": [
+      "file",
+      "upload",
+      ""
+    ],
+    "headers": {
+      "content-type": "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "*/*",
+      "Cache-Control": "no-cache",
+      "Host": hostname,
+      "accept-encoding": "gzip, deflate",
+      "content-length": "179",
+      "Connection": "keep-alive",
+      "cache-control": "no-cache"
+    }
+  }
+
+  return new Promise (async function (resolve, reject) {
+    var req = http.request(options, function (res) {
+      var chunks = [];
+
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+
+      res.on("end", function () {
+        var body = Buffer.concat(chunks);
+        console.log(body.toString());
+      });
+    });
+
+    req.write("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"remark \"\r\n" + remark + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--");
+    req.end()
+  })
+}
 
 fdk.handle(function(input){
-  let name = 'World';
-  if (input.name) {
-    name = input.name;
-  }
-  return {'message': 'Hello ' + name}
+  var results = {}
+  var message = 'Drone triggered to ' + input.hostname + " with remark " + input.remark
+  callDrone(input.hostname, input.remark)
+  .then(res => {
+    console.log("triggered call to proxy " + hostname + " with remark " + remark)
+    resolve("Success!")
+  })
+  .catch(e => {
+    console.log("Proxy not triggered properly")
+    reject(e)
+  })
+  return results
+
 })
