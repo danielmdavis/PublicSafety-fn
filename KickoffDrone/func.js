58fce7a1e7bb5b8ce302757d1bacc98f94bb4dc5
Shane Landrum <shane.landrum@oracle.com>
2019-07-18 13:53:25 -0400

    Not quite working perfectly, but committing this for records.

--- KickoffDrone/func.js
+++ KickoffDrone/func.js
@@ -30,13 +30,11 @@ async function callDrone(hostname, remark) {
 //  }
 
   return new Promise( async function (resolve, reject) {
+    var json = {}
     await fetch(uri, options)
       .then(function(res) {
-        myJSON = res.json()
-//        console.log("callDrone: JSON is " + myJSON)
-//        return myJSON
+        resolve(res.body.json())
       })
-      .then(function(json) {resolve(json) })
       .catch(e => {
         console.log('error in callDrone: ' + e)
         reject(e)
@@ -49,8 +47,9 @@ fdk.handle(function(input){
   var results = {}
   var message = 'Drone triggered to ' + input.hostname + " with remark " + input.remark
   callDrone(input.hostname, input.remark)
-  .then(res => {
+  .then(json => {
      results = "KickoffDrone: Handler triggered call to proxy " + hostname + " with remark " + remark
+     return json
   })
   .catch(e => {
      results = "KickoffDrone: proxy not triggered properly"
